f66b399f92963061928f271483e43e47f91f9d0d
khan2626 <israelkhan217@gmail.com>
2023-11-24 23:13:06 +0100

    1-install_a_package.pp update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 #!/usr/bin/env puppet
 # Installs a specific (2.1.0) version of flask
 
-package { 'flask':
+package { 'werkzeug':
   ensure   => '2.1.0',
+  name     => 'Flask',
   provider => 'pip3',
 }
